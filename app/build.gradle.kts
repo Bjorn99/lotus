d5bf4a82c06a248e03f3c7ddec5dbe0715915b6b
Bjorn99 <67769176+Bjorn99@users.noreply.github.com>
2026-06-15 20:43:49 +0545

    chore: bump version to 1.7.1-community (1_007_001)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.dn0ne.lotus.community"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1_007_000
-        versionName = "1.7.0-community"
+        versionCode = 1_007_001
+        versionName = "1.7.1-community"
 
         if (splitApks) {
             splits {
